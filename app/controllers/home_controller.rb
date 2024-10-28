244f8cccd37874f7babf2040494e7ecee33e0f76
Ricardo Gallegos <ricardo.gallegos@usach.cl>
2024-10-28 07:13:47 -0300

    publications controller methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
   end
 
+  # si está loggeado se va a las publicaciones
   def is_logged
     if user_signed_in?
       redirect_to publications_index_path
--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,18 +1,65 @@
 class PublicationsController < ApplicationController
   before_action :authorize_admin
-  before_action :authenticate_user!
+  #  before_action :authenticate_user!
+  before_action :set_publication, only: %i[ show edit ]
   def index
+    @publications = Publication.all.order(id: :desc)
   end
 
   def show
+    # before_action
+  end
+
+  def edit
+    # before_action
   end
 
   def update
+    respond_to do |format|
+      if @publication.update(publication_params)
+        format.html { redirect_to @publication, notice: "Publication was successfully updated." }
+        format.json { render :show, status: :ok, location: @publication }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create
+    @publication = Publication.new(publication_params)
+    @publication.user = current_user
+
+    respond_to do |format|
+      if @publication.save
+        format.html { redirect_to @publication, notice: "Publication was successfully created." }
+        format.json { render :show, status: :created, location: @publication }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  def destroy
+    @publication.destroy!
+    respond_to do |format|
+      format.html { redirect_to publications_path, status: :see_other, notice: "Publcacion was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def publication_params
+    params.require(:publication).permit(:title, :content, :id_user)
+  end
+
+  def set_publication
+    @publication = Publication.find(params[:id])
+  end
+
+  # si no está loggeado se va al Home_index
   def authorize_admin
     if current_user == nil
       redirect_to root_path
